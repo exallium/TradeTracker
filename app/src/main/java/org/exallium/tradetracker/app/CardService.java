9b127e8c54e181019bd8c95f8c432491efea94c5
Alex Hart <alex.hart@redspace.com>
2015-04-15 14:30:15 -0300

    Add code for adding card data

--- app/src/main/java/org/exallium/tradetracker/app/CardService.java
+++ app/src/main/java/org/exallium/tradetracker/app/CardService.java
@@ -7,14 +7,22 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
 import com.orm.query.Condition;
 import com.orm.query.Select;
 import org.exallium.tradetracker.app.model.entities.Card;
 import org.exallium.tradetracker.app.model.entities.CardSet;
 import org.exallium.tradetracker.app.model.rest.RestServiceManager;
+import rx.Observable;
 import rx.Subscriber;
 import rx.functions.Action0;
+import rx.schedulers.Schedulers;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CardService extends Service {
@@ -72,7 +80,65 @@ public class CardService extends Service {
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(NOTIFICATION_ID, builder.build());
 
-        diskLoadComplete();
+        Observable.create(subscriber -> {
+
+            InputStream jsonIn = getResources().openRawResource(R.raw.card_data);
+
+            try {
+                Gson gson = new Gson();
+                JsonReader reader = new JsonReader(new InputStreamReader(jsonIn));
+                reader.beginArray();
+                reader.beginArray();
+
+                List<CardSet> cardSets = new ArrayList<>();
+                while (reader.hasNext()) {
+                    CardSetJsonObject cardSetJsonObject = gson.fromJson(reader, CardSetJsonObject.class);
+                    CardSet cardSet = Select.from(CardSet.class).where(Condition.prop("code").eq(cardSetJsonObject.code)).first();
+
+                    if (cardSet == null) {
+                        cardSet = new CardSet();
+                        cardSet.count = cardSetJsonObject.count;
+                        cardSet.code = cardSetJsonObject.code;
+                        cardSets.add(cardSet);
+                    }
+
+                }
+
+                CardSet.saveInTx(cardSets);
+
+                reader.endArray();
+                reader.beginArray();
+
+                List<Card> cards = new ArrayList<>();
+                while (reader.hasNext()) {
+                    CardJsonObject cardJsonObject = gson.fromJson(reader, CardJsonObject.class);
+                    Card card = Select.from(Card.class)
+                            .where(Condition.prop("cardSet.code").eq(cardJsonObject.set), Condition.prop("name").eq(cardJsonObject.name))
+                            .first();
+
+                    if (card == null) {
+                        CardSet cardSet = Select.from(CardSet.class).where(Condition.prop("code").eq(cardJsonObject.set)).first();
+                        card = new Card();
+                        card.cardSet = cardSet;
+                        card.name = cardJsonObject.name;
+                        cards.add(card);
+                    }
+                }
+
+                Card.saveInTx(cards);
+
+                reader.endArray();
+                reader.endArray();
+
+                reader.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }).onBackpressureBlock()
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.io()).doOnCompleted(CardService.this::diskLoadComplete).subscribe();
     }
 
     private void diskLoadComplete() {
